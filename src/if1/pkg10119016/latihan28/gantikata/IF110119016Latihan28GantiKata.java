566ae110e648f8d394e8a2d1eb23cc30152ef0c3
aditi <aditi@DESKTOP-343N0FS>
2020-10-31 00:48:28 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan28/gantikata/IF110119016Latihan28GantiKata.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan28.gantikata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan program
+ * mengganti kata
+ */
+public class IF110119016Latihan28GantiKata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String hasil = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====Program Mengganti Kata====\n");
+        System.out.print("Masukkan Kalimat : ");
+        String kalimat = scanner.nextLine();
+        System.out.print("Ganti Kata : ");
+        String kata_lama = scanner.nextLine();
+        System.out.print("Menjadi Kata : ");
+        String kata_baru = scanner.nextLine();
+        String[] perkata = kalimat.split(" ");
+        for (String kata : perkata) {
+            if (kata.equalsIgnoreCase(kata_lama)) {
+                hasil += kata_baru + " ";
+            }
+            else {
+                hasil += kata + " ";
+            }
+        }
+        System.out.println("Kalimat Baru : "+hasil.trim());
+    }
+}
